3ca56df03e56c29e6cdb2f8a338791798ae1ec41
Chirag Makwana <chiragmakwanadev@gmail.com>
2024-12-18 11:31:01 +0530

    changed header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { FaXTwitter } from "react-icons/fa6";
-import { FaFacebookF, FaInstagram } from "react-icons/fa";
-import { montserrat } from "@/pages";
 import Link from "next/link";
 import { PolicyData } from "@/static/PolicyData";
 import { Data as StaffData } from "@/static/StaffData";
@@ -54,39 +51,38 @@ const Header = () => {
 
   return (
     <div
-      className={`backdrop-blur-md bg-black/30 text-white flex items-center px-[15px] md:px-[20px] xl:px-[40px] 
-    py-[12px] xl:py-[20px] fixed justify-between w-full z-50 ${montserrat.className}`}
+      className={`backdrop-blur-md bg-white/50 text-black flex items-center justify-between px-5 py-2 w-auto z-50 rounded-2xl`}
+      style={{
+        position: "fixed",
+        left: "20px",
+        top: "20px",
+        right: "20px !important",
+      }}
     >
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-3 items-center w-[22%]">
         <Link href="/">
-          <img
-            src="/images/logo-nobg.png"
-            alt="logo"
-            className="w-[250px]"
-            style={{ width: Clamp(8, 15) }}
-          />
+          <img src="/images/logo.png" alt="logo" className="w-[50px]" />
         </Link>
       </div>
       <div className="flex xl:hidden">
         <HamburgerMenu />
       </div>
-      <ul
-        className="hidden xl:flex gap-1 2xl:gap-2 font-medium text-[12px] 2xl:text-[16px] "
-        // style={{ fontSize: Clamp(0.7, 1) }}
-      >
+      <ul className="hidden xl:flex gap-1 2xl:gap-7 font-medium text-[12px] 2xl:text-[16px]">
+        {/* OUR STAFF */}
         <li
-          className="border border-transparent hover:border-white duration-500 rounded-md px-[15px] py-[5px] relative"
+          className="group relative py-[5px] cursor-pointer duration-300"
           onMouseEnter={handleStaffMouseEnter}
           onMouseLeave={handleStaffMouseLeave}
         >
           <Link href="/staff">OUR STAFF</Link>
+          <span className="absolute bottom-0 left-0 h-[2px] bg-black w-0 group-hover:w-full transition-all duration-300"></span>
           {isStaffHovering && (
-            <div className="absolute left-0 top-[100%] bg-[#0f68df] text-white shadow-lg rounded-md mt-2 w-[200px] z-10">
+            <div className="absolute left-0 top-[100%] backdrop-blur-xl bg-black/70 text-white shadow-lg rounded-md mt-2 w-[200px] z-10 overflow-hidden">
               <ul className="space-y-2">
                 {doctors.map((doctor) => (
                   <li
                     key={doctor.slug}
-                    className="hover:text-gray-400 px-4 py-2"
+                    className="hover:text-black hover:bg-white  px-4 py-2"
                   >
                     <Link href={`/staff/${doctor.slug}`}>{doctor.name}</Link>
                   </li>
@@ -94,7 +90,7 @@ const Header = () => {
                 {pharmacists.map((pharmacist) => (
                   <li
                     key={pharmacist.slug}
-                    className="hover:text-gray-400 px-4 py-2"
+                    className="hover:text-black hover:bg-white  px-4 py-2"
                   >
                     <Link href={`/staff/${pharmacist.slug}`}>
                       Pharmacist {pharmacist.name}
@@ -105,17 +101,23 @@ const Header = () => {
             </div>
           )}
         </li>
+
+        {/* CLINIC POLICIES */}
         <li
-          className="border border-transparent hover:border-white duration-500 rounded-md px-[15px] py-[5px] relative"
+          className="group relative py-[5px] cursor-pointer duration-300"
           onMouseEnter={handlePolicyMouseEnter}
           onMouseLeave={handlePolicyMouseLeave}
         >
           <Link href="/policy/clinic-policies">CLINIC POLICIES</Link>
+          <span className="absolute bottom-0 left-0 h-[2px] bg-black w-0 group-hover:w-full transition-all duration-300"></span>
           {isPolicyHovering && (
-            <div className="absolute left-0 top-[100%] bg-[#0f68df] text-white shadow-lg rounded-md mt-2 w-[200px] z-10">
+            <div className="absolute left-0 top-[100%] backdrop-blur-xl bg-black/70 overflow-hidden text-white shadow-lg rounded-md mt-2 w-[200px] z-10">
               <ul className="space-y-2">
                 {policyTopics.map((topic) => (
-                  <li key={topic} className="hover:text-gray-400 px-4 py-2">
+                  <li
+                    key={topic}
+                    className="hover:text-black hover:bg-white  px-4 py-2"
+                  >
                     <Link href={`/policy/${PolicyData[topic][0].slug}`}>
                       {topic}
                     </Link>
@@ -125,42 +127,43 @@ const Header = () => {
             </div>
           )}
         </li>
-        <li className="border border-transparent hover:border-white duration-500 rounded-md px-[15px] py-[5px]">
+
+        {/* NEWS */}
+        <li className="group relative py-[5px] cursor-pointer duration-300">
           <Link href="/news">NEWS</Link>
+          <span className="absolute bottom-0 left-0 h-[2px] bg-black w-0 group-hover:w-full transition-all duration-300"></span>
         </li>
-        <li className="border border-transparent hover:border-white duration-500 rounded-md px-[15px] py-[5px]">
+
+        {/* CONTACT */}
+        <li className="group relative py-[5px] cursor-pointer duration-300">
           <Link href="/contact">CONTACT US</Link>
+          <span className="absolute bottom-0 left-0 h-[2px] bg-black w-0 group-hover:w-full transition-all duration-300"></span>
         </li>
-        <li className="border border-transparent hover:border-white duration-500 rounded-md px-[15px] py-[5px]">
+
+        {/* BECOME A PATIENT */}
+        <li className="group relative py-[5px] cursor-pointer duration-300">
           <Link href="/news/4">BECOME A PATIENT</Link>
+          <span className="absolute bottom-[5px] left-0 h-[2px] bg-black w-0 group-hover:w-full transition-all duration-300"></span>
         </li>
-        <li className="border border-transparent hover:border-white duration-500 rounded-md px-[15px] py-[5px] bg-red-500">
+
+        {/* BOOK APPOINTMENT */}
+      </ul>
+      <ul className="flex items-center gap-3">
+        <li
+          className="group relative px-[20px] py-[5px] bg-transparent border-black border-[1px] hover:bg-black
+         hover:text-white cursor-pointer duration-300 rounded-full"
+        >
           <Link href="https://ocean.cognisantmd.com/intake/patients.html?linkRef=04f9eeaf-8434-4153-aa26-2d0c910f4020#/online-booking">
             BOOK APPOINTMENT
           </Link>
         </li>
-        <li className="border border-transparent hover:border-white duration-500 rounded-md px-[15px] py-[5px] bg-red-500">
+
+        {/* PHARMACY */}
+        <li
+          className="group relative px-[20px] py-[5px] bg-transparent border-black border-[1px] hover:bg-black
+         hover:text-white cursor-pointer duration-300 rounded-full"
+        >
           <Link href="https://www.bookmyshot.com/9054351100">PHARMACY</Link>
-        </li>
-      </ul>
-      <ul
-        className="gap-4 hidden xl:flex"
-        style={{ fontSize: Clamp(0.4, 1.25) }}
-      >
-        <li>
-          <Link href="https://x.com/vmcwhitby">
-            <FaXTwitter />
-          </Link>
-        </li>
-        <li>
-          <Link href="https://www.facebook.com/profile.php?id=100094622814558">
-            <FaFacebookF />
-          </Link>
-        </li>
-        <li>
-          <Link href="https://www.instagram.com/valleyviewmedical/">
-            <FaInstagram />
-          </Link>
         </li>
       </ul>
     </div>
