866f740fda72a23d547bb463dfc80d9f52f444e7
Chirag Makwana <chiragmakdev@gmail.com>
2025-03-20 11:46:23 +0530

    check101

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -63,21 +63,43 @@ const Footer = () => {
           <ul style={{ fontSize: Clamp(0.75, 1.25) }}>
             <li>
               <span className=" font-semibold">CALL US:</span>
-              <Link href="tel:+19054341662" className="hover:text-red-600">
-                +1 905-434-1662
-              </Link>
+              {locationAccess === "Whitby" ? (
+                <Link href="tel:+19054341662" className="hover:text-red-600">
+                  +1 905-434-1662
+                </Link>
+              ) : (
+                <Link href="tel:+19053678510" className="hover:text-red-600">
+                  +1 905-367-8510
+                </Link>
+              )}
             </li>
-            <li>
-              FAX US: <span className="font-semibold">+1 905-747-0999</span>
-            </li>
+            {locationAccess === "Whitby" ? (
+              <li>
+                FAX US: <span className="font-semibold">+1 905-747-0999</span>
+              </li>
+            ) : (
+              <li>
+                FAX US: <span className="font-semibold">289-764-1231</span>
+              </li>
+            )}
+
             <li className="font-semibold">
               <span>EMAIL US:</span>
-              <Link
-                href="mailto:info@valleyviewmedical.ca"
-                className="hover:text-red-600"
-              >
-                info@valleyviewmedical.ca
-              </Link>
+              {locationAccess === "Whitby" ? (
+                <Link
+                  href="mailto:info@valleyviewmedical.ca"
+                  className="hover:text-red-600"
+                >
+                  info@valleyviewmedical.ca
+                </Link>
+              ) : (
+                <Link
+                  href="mailto:info@valleyviewmedical.ca"
+                  className="hover:text-red-600"
+                >
+                  harmonypharmachoice@gmail.com
+                </Link>
+              )}
             </li>
           </ul>
         </div>
